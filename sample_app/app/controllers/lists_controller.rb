1f754c67216964d071ea724dc014a977c9797b1c
EC2 Default User <ec2-user@ip-172-31-9-99.ec2.internal>
2022-05-27 15:36:13 +0000

    app_comp-practice-11

--- sample_app/app/controllers/lists_controller.rb
+++ sample_app/app/controllers/lists_controller.rb
@@ -6,22 +6,38 @@ class ListsController < ApplicationController
   
   def create
     # データを受け取り、新規登録するためのインスタンス作成
-    list = List.new(list_params)
+    @list = List.new(list_params)
     # データをデータベースにほぞんするためのsaveメソッドの実行
-    list.save
-    # トップ画面へリダイレクト
-    redirect_to '/top'
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
   end
   
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+  
+  def destroy
+    list = List.find(params[:id])     # findにより、データ(レコード)を一件取得
+    list.destroy                      # データを削除
+    redirect_to '/lists'              # 投稿一覧画面へリダイレクト
+  end
   
   private # これより下に記述すると、アクションとして認識されなくなり、
           # メソッドの呼び出しを制限できる。
@@ -29,7 +45,7 @@ class ListsController < ApplicationController
   # ストロングパラメータ
   def list_params         # 本来であればアクションとして認識し、URLで呼び出せてしまう。
   # praivateメソッドは「モデル名_params」でする
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
   
 end
